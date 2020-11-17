1112aab863e6073285e6d4710c39a2b0e227c6b3
Brendonk13 <brendonk13.bk@gmail.com>
2020-11-16 18:02:13 -0700

    1 more flow left on story 7

--- brendon_features/steps/story07_steps.py
+++ brendon_features/steps/story07_steps.py
@@ -3,28 +3,57 @@ import requests
 import logging
 
 
-@given('The class {class_id} exists')
-def step_impl(context, class_id):
+def get_done_status(doneStatus):
+    return True if doneStatus == 'true' else False
+
+
+def get_all_todos(context):
+    return requests.get(context.base_url + 'todos')
+
+
+def get_projects(context):
     obj_name = 'projects'
     logging.info('before checking project exists')
     response = requests.get(context.base_url + obj_name)
     response_body = response.json()[obj_name]
     logging.info('after checking project exists, response: {}'.format(response_body))
-
-    found_project = any(
-            project['id'] == class_id
-            for project in response_body
-    )
-
-    assert found_project
     assert response.ok
 
+    return response_body
 
-@given('The class has an incomplete task with title {title}')
-def step_impl(context, title):
+
+def value_in_response(searched_for_value, response_body, key=None):
+    return any(
+        obj[key] == searched_for_value
+        for obj
+        in response_body
+    )
+
+
+def project_exists(project_id, projects):
+    return value_in_response(project_id, projects, key='id')
+    # return any(
+    #     project['id'] == project_id
+    #     for project in projects
+    # )
+
+
+
+
+
+
+@given('The class {project_id} exists')
+def step_impl(context, project_id):
+    assert project_exists(project_id, get_projects(context))
+
+
+@given('The class has an {doneStatus} task with title {title}')
+def step_impl(context, doneStatus, title):
     obj_name = 'projects/1/tasks'
-    json = {'title': title, 'doneStatus': False}
-    logging.info('before creating incomplete task with params: {}'.format(json))
+    done_status = get_done_status(doneStatus)
+
+    json = {'title': title, 'doneStatus': done_status}
+    logging.info('before creating {} task with params: {}'.format(done_status, json))
     response = requests.post(context.base_url + obj_name, json=json)
 
     logging.info('after post request, response: {}'.format(response))
@@ -33,7 +62,7 @@ def step_impl(context, title):
     assert response.ok
 
     created_id = response_body['id']
-    logging.info('incomplete task created_id: {}'.format(created_id))
+    logging.info('{} task created_id: {}'.format(done_status, created_id))
 
     context.prev_created_id = created_id
     # this obj will be deleted after the scenario
@@ -41,28 +70,39 @@ def step_impl(context, title):
     context.created_ids['todos'].append(created_id)
 
 
-
-@when('I view all incomplete tasks')
-def step_impl(context):
-    response = requests.get(context.base_url + 'todos')
-    context.incomplete_tasks = response.json()['todos']
-    logging.info('all incomplete tasks: {}'.format(context.incomplete_tasks))
+@when('I view all {doneStatus} tasks')
+def step_impl(context, doneStatus):
+    done_status = get_done_status(doneStatus)
+    response = get_all_todos(context)
+    context.tasks = response.json()['todos']
+    logging.info('all {} tasks: {}'.format(done_status, context.tasks))
     assert response.ok
 
 
-@then('The incomplete task with title {title} should exist and be marked incomplete')
-def step_impl(context, title):
+@then('The task with title {title} will be present and be marked {doneStatus}')
+def step_impl(context, title, doneStatus):
+    done_status = get_done_status(doneStatus)
     logging.info('')
     logging.info('')
-    for task in context.incomplete_tasks:
+    for task in context.tasks:
         if all((
                 context.prev_created_id == task['id'],
                 title == task['title'],
                 )):
-            logging.info('found the incomplete task')
-            assert task['doneStatus'] == 'false'
-            logging.info('doneStatus is false!')
+            logging.info('found the {} task'.format(done_status))
+            assert task['doneStatus'] == doneStatus
+            logging.info('doneStatus is {}!'.format(done_status))
             return
 
     # occurs if we don't find task and hence don't return
     assert False
+
+
+
+
+# @when('I view all complete tasks')
+# def step_impl(context):
+#     response = get_all_todos(context)
+#     context.complete_tasks = response.json()['todos']
+#     logging.info('all complete tasks: {}'.format(context.incomplete_tasks))
+#     assert response.ok
--- brendon_features/story07_query_incomplete_tasks.feature
+++ brendon_features/story07_query_incomplete_tasks.feature
@@ -6,27 +6,41 @@ Feature: Query incomplete tasks
 
 
 Scenario Outline: (Normal flow) Query all incomplete tasks
-    Given The class <class_id> exists
-    And The class has an incomplete task with title <title>
-    When I view all incomplete tasks
-    Then The incomplete task with title <title> should exist and be marked incomplete
+    Given The class <project_id> exists
+    And The class has an <doneStatus> task with title <title>
+    When I view all <doneStatus> tasks
+    Then The task with title <title> will be present and be marked <doneStatus>
 
     Examples: Incomplete Tasks
-        | class_id | title |
-        | 1        | task1 |
-        | 1        | task2 |
-        | 1        | task3 |
+        | project_id | title | doneStatus |
+        | 1          | task1 | false      |
+        | 1          | task2 | false      |
+        | 1          | task3 | false      |
 
 
-# Scenario Outline: (Alternate flow) Query all incomplete tasks
-#     Given There exists an incomplete task with title <title>
-#         When I view all incomplete tasks
-#         Then The incomplete task with title <title> should exist and be marked incomplete
+# Scenario Outline: (Alternate flow) Query all complete tasks
+    Given The class <project_id> exists
+    And The class has an <doneStatus> task with title <title>
+    When I view all <doneStatus> tasks
+    Then The task with title <title> will be present and be marked <doneStatus>
+
+    Examples: Incomplete Tasks
+        | project_id | title | doneStatus |
+        | 1          | task1 | true       |
+        | 1          | task2 | true       |
+        | 1          | task3 | true       |
+
+
+# Scenario Outline: (Error flow) Query all incomplete tasks
+#     Given The class <project_id> exists
+#     And The class has an incomplete task with title <title>
+#     When I view all incomplete tasks
+#     Then The task with title <title> should exist and be marked incomplete
 # 
 #     Examples: Incomplete Tasks
-#         | title |
-#         | task1 |
-#         | task2 |
-#         | task3 |
-
-
+#         | project_id | title |
+#         | 1          | task1 |
+#         | 1          | task2 |
+#         | 1          | task3 |
+# 
+# 
